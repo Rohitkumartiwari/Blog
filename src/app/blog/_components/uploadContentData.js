f8f8c5d7f846aaf0cbad8b8cd1d1b8cf22697f3f
Rohitkumartiwari <sonutiwari.july@gmail.com>
2024-07-04 11:46:32 +0530

    chmages done

--- src/app/blog/_components/uploadContentData.js
+++ src/app/blog/_components/uploadContentData.js
@@ -18,7 +18,7 @@ const UploadContentData = () => {
   const [data, setData] = useState([]);
   const [likeBlog, setLike] = useState(false);
   const [fetchBlog, setFetchBlog] = useState(false);
-  const [commentOpen, setCommentOpen] = useState(false);
+  const [commentOpen, setCommentOpen] = useState(-1);
   const [loading, setLoading] = useState(false);
   const obj=useContext(PageContext);
   const[auth,setAuth]=useState();
@@ -104,7 +104,7 @@ const UploadContentData = () => {
   };
   const CommentBoxOpen=(item)=>
     {
-      setCommentOpen(!commentOpen)
+      setCommentOpen(prev=>prev===item.post_id?-1:item.post_id)
     }
   const postComment=(item)=>
     {
@@ -116,7 +116,7 @@ const UploadContentData = () => {
         comment:comment
       })
       .then((response) => {
-        setCommentOpen(!commentOpen)
+        setCommentOpen(-1)
         console.log(response);
       })
       .catch((error) => {
@@ -135,154 +135,153 @@ const UploadContentData = () => {
         });
     }, []);
    
-  
+  console.log(commentOpen,"idddd");
   return (
     <div>
       {loading?<LoadingComponent/>:<>{data?.map((item) => {
+        console.log(item,"itemmm");
         return (
-          <>
-          
-            <Card key={item?.id}>
-              <CardBody>
-                <div className="d-flex justify-content-between">
-                  <div className="d-flex align-items-center gap-2 align-items-center">
-                    <div className={Styles.blog_header_image}>
-                      
-                      <Image
-                        src={
-                          item?.profile_img
-                            ? `/uploads/${item?.profile_img}`
-                            : user
-                        }
-                        alt="img not found"
-                        width={50}
-                        height={50}
-                      />
-                    </div>
-                    <div>
-                      <h5 className="mb-0">{item?.name}</h5>
-                      <p className="mb-0 fs-6">{item?.email}</p>
-                    </div>
-                  </div>
-                  <p>
-                    <MdDelete
-                      size={20}
-                      color="red"
-                      onClick={() => deleteBlog(item)}
-                      className="cursor-pointer"
-                    />
-                  </p>
-                </div>
-
-                <p className="my-2">{item?.content}</p>
-                <div
-                  className={`${Styles.blog_image} d-flex justify-content-center`}
-                >
-                  <img
-                    src={`http://localhost:3000/uploads/${item?.image_filename}`}
+         
+          <Card key={item?.id}>
+          <CardBody>
+            <div className="d-flex justify-content-between">
+              <div className="d-flex align-items-center gap-2 align-items-center">
+                <div className={Styles.blog_header_image}>
+                  
+                  <Image
+                    src={
+                      item?.profile_img
+                        ? `/uploads/${item?.profile_img}`
+                        : user
+                    }
                     alt="img not found"
-                    width="100%"
-                    height={506}
-                    className={Styles.img_property}
+                    width={50}
+                    height={50}
                   />
                 </div>
-                <div className="my-1 d-flex justify-content-between">
-                  <div>
-                    {filterData?.find((it) => it?.post_id == item?.post_id) ? (
-                      <BiSolidLike size={23} color="blue"/>
-                    ) : (
-                      <BiSolidLike
-                        size={23}
-                        className="cursor-pointer"
-                        onClick={() => {
-                          like(item);
-                          
-                        }}
-                      />
-                    )}
-                    <span className="ms-2 mb-0">
-                      {" "}
-                      {totalLike(item) != 0 && (
-                        <>Total Like {totalLike(item)}</>
-                      )}
-                    </span>
-                  </div>
-
-                  <div>
-                    <MdMessage
-                      size={25}
-                      className="cursor-pointer"
-                      onClick={() => CommentBoxOpen(item)}
-                    />
-                    <span className="ms-2"> 
-
-                    {totalComment(item) != 0 && (
-                        <>Total Comment {totalLike(item)+1}</>
-                      )}
-
-                    </span>
-                  </div>
+                <div>
+                  <h5 className="mb-0">{item?.name}</h5>
+                  <p className="mb-0 fs-6">{item?.email}</p>
                 </div>
-                {commentOpen && (
-                  <div className="d-flex gap-2 align-items-center">
-                    <div className={Styles.blog_header_image}>
-                      <Image
-                        src={
-                          item?.profile_img
-                            ? `/uploads/${item?.profile_img}`
-                            : user
-                        }
-                        alt="img not found"
-                        width={40}
-                        height={35}
-                      />
-                    </div>
-                    <div className="w-100">
-                    {" "}
-                    <Input type="text" placeholder="Add a Comment" onChange={(e)=>setComment(e.target.value)}/>
-                   {" "}
-                  </div>
-                  <Button onClick={()=>postComment(item)} >
-                    Post
-                  </Button>
-                  </div>
-                  
+              </div>
+              <p>
+                <MdDelete
+                  size={20}
+                  color="red"
+                  onClick={() => deleteBlog(item)}
+                  className="cursor-pointer"
+                />
+              </p>
+            </div>
+
+            <p className="my-2">{item?.content}</p>
+            <div
+              className={`${Styles.blog_image} d-flex justify-content-center`}
+            >
+              <img
+                src={`http://localhost:3000/uploads/${item?.image_filename}`}
+                alt="img not found"
+                width="100%"
+                height={506}
+                className={Styles.img_property}
+              />
+            </div>
+            <div className="my-1 d-flex justify-content-between">
+              <div>
+                {filterData?.find((it) => it?.post_id == item?.post_id) ? (
+                  <BiSolidLike size={23} color="blue"/>
+                ) : (
+                  <BiSolidLike
+                    size={23}
+                    className="cursor-pointer"
+                    onClick={() => {
+                      like(item);
+                      
+                    }}
+                  />
                 )}
+                <span className="ms-2 mb-0">
+                  {" "}
+                  {totalLike(item) != 0 && (
+                    <>Total Like {totalLike(item)}</>
+                  )}
+                </span>
+              </div>
+
+              <div>
+                <MdMessage
+                  size={25}
+                  className="cursor-pointer"
+                  onClick={() => CommentBoxOpen(item)}
+                />
+                <span className="ms-2"> 
+
+                {totalComment(item) != 0 && (
+                    <>Total Comment {totalLike(item)+1}</>
+                  )}
+
+                </span>
+              </div>
+            </div>
+            {commentOpen === item.post_id && (
+              <div className="d-flex gap-2 align-items-center">
+                <div className={Styles.blog_header_image}>
+                  <Image
+                    src={
+                      item?.profile_img
+                        ? `/uploads/${item?.profile_img}`
+                        : user
+                    }
+                    alt="img not found"
+                    width={40}
+                    height={35}
+                  />
+                </div>
+                <div className="w-100">
+                {" "}
+                <Input type="text" placeholder="Add a Comment" onChange={(e)=>setComment(e.target.value)}/>
+               {" "}
+              </div>
+              <Button onClick={()=>postComment(item)} >
+                Post
+              </Button>
+              </div>
+              
+            )}
+            <>
+            {commentData?.map((it)=>
+            {
+              if(it?.post_id==item?.post_id)
+              return(
                 <>
-                {commentData?.map((it)=>
-                {
-                  if(it?.post_id==item?.post_id)
-                  return(
-                    <>
-                    <div className="d-flex gap-2 align-items-center">
-                    <div className={Styles.blog_header_image}>
-                      <Image
-                        src={
-                          it?.profile_img
-                            ? `/uploads/${it?.profile_img}`
-                            : user
-                        }
-                        alt="img not found"
-                        width={35}
-                        height={35}
-                      />
-                    </div>
-                    <div className="w-100">
-                    {" "}
-                    <p className="mb-0 fw-bold">{it.name}</p>
-                   <p className="mb-0">{it?.comment}</p>
-                   
-                  </div>
-                  </div>
-                    </>
-                  )
-                })}
-                
-                
+                <div className="d-flex gap-2 align-items-center">
+                <div className={Styles.blog_header_image}>
+                  <Image
+                    src={
+                      it?.profile_img
+                        ? `/uploads/${it?.profile_img}`
+                        : user
+                    }
+                    alt="img not found"
+                    width={35}
+                    height={35}
+                  />
+                </div>
+                <div className="w-100">
+                {" "}
+                <p className="mb-0 fw-bold">{it.name}</p>
+               <p className="mb-0">{it?.comment}</p>
+               
+              </div>
+              </div>
                 </>
-              </CardBody>
-            </Card>
-          </>
+              )
+            })}
+            
+            
+            </>
+          </CardBody>
+        </Card>
         );
       })}</>}
       
